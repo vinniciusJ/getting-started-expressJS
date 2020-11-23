8c07b9ed088e5ca04b6ad66ac209496f3310ad3e
Vinicius <vinnicius.jimenez@gmail.com>
2020-11-23 09:57:55 -0300

    Hello World

---
+++ hello-world/app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => response.send('Hello World'))
+
+app.listen(port, () => console.log(`Example app listenning at http://localhost:${port}`))
